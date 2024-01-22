44efca6c6be947209286dfb38a3986d7483d2deb
Kevin Marlei <kevin_SSDP@hotmail.com>
2024-01-22 17:46:41 +0100

    Lado esquerdo refeito

--- js/main2.js
+++ js/main2.js
@@ -1,38 +1,179 @@
 const apiKey = '38f1b1a71474f8940ad30733832dcaea';
 const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
-const options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0,
+
+async function apiData(local, estado, pais) {
+  try {
+    const response = await axios.get(API_URL, {
+      params: {
+        q: `${local}${estado ? ',' + estado : ''},${pais}`,
+        appid: apiKey,
+        lang: 'pt_br',
+        units: 'metric',
+      }
+
+    });
+    const responseData = response.data;
+    console.log(responseData);
+    renderizarLadoEsquerdo(responseData);
+  } catch (error) {
+    console.error('Erro na requisição api', error);
+  }
+}
+async function localStorageReserv(local, estado, pais) {
+  try {
+    const response = await axios.get(API_URL, {
+      params: {
+        q: `${local}${estado ? ',' + estado : ''},${pais}`,
+        appid: apiKey,
+        lang: 'pt_br',
+        units: 'metric',
+      }
+
+    });
+    const responseData = response.data;
+    console.log(responseData);
+
+  } catch (error) {
+    console.error('Erro na requisição api', error);
+  }
+}
+
+let localidade;
+//caixa img-clima
+const nomeLocal = document.querySelector('.nome-local');
+const pais = document.querySelector('.pais');
+const imgClima = document.querySelector('.icon-clima');
+const descricaoClima = document.querySelector('.descricao-clima');
+//caixa de busca
+const caixaBusca = document.querySelector('.caixa-busca');
+const adicionarLocal = document.querySelector('.img-adicionar');
+const inputBusca = document.querySelector('.input-busca');
+const btnBusca = document.querySelector('.img-busca');
+//caixa temperatura
+const imgPositivo = document.querySelector('.positivo');
+const imgNegativo = document.querySelector('.negativo');
+const temperaturaAtual = document.querySelector('.temperatura-atual');
+//caixa min-max que só contém a sensação térmica
+const sensacaoTermica = document.querySelector('.sensacao-termica');
+//caixa detalhes
+const ventos = document.querySelector('.vento');
+const humidade = document.querySelector('.humidade');
+const visibilidade = document.querySelector('.visibilidade');
+//elementos criados dinâmicamente para favoritar local e renderizar dados ao iniciar seção
+const inputCriado = document.createElement('input');
+const botaoOK = document.createElement('button');
+const cancelarAdd = document.createElement('button');
+const imgCancelar = document.createElement('img');
+//background geral do lado esquerdo
+const ladoEsquerdoBackground = document.querySelector('.lado-esquerdo')
+
+
+function renderizarAddClick() {
+  adicionarLocal.addEventListener('click', (event) => {
+    event.preventDefault();
+    caixaBusca.appendChild(inputCriado);
+    caixaBusca.appendChild(botaoOK);
+    cancelarAdd.appendChild(imgCancelar)
+    caixaBusca.appendChild(cancelarAdd);
+    inputCriado.placeholder = 'Adicione ou modifique o local desejado'
+    imgCancelar.src = './icon/cancelar.svg';
+    botaoOK.textContent = 'OK'
+    inputBusca.style.display = 'none';
+    btnBusca.style.display = 'none';
+    adicionarLocal.style.display = 'none';
+  });
 };
 
-function success(pos) {
-  const local = pos.coords;
-  console.log('Latitude JS:', local.latitude);
-  console.log('Longitude JS:', local.longitude);
-  const latitude = local.latitude;
-  const longitude = local.longitude;
-  apiData(latitude, longitude)
-}
+function desrenderizarAddClick() {
+  cancelarAdd.addEventListener('click', (event) => {
+    event.preventDefault();
+    caixaBusca.removeChild(inputCriado);
+    caixaBusca.removeChild(botaoOK);
+    caixaBusca.removeChild(cancelarAdd);
+    cancelarAdd.removeChild(imgCancelar);
 
-navigator.geolocation.watchPosition(success, error, options);
-function error(){
-
-}
-async function apiData(lat, lon){
-  const response = await axios.get(API_URL, {
-    params:{
-      lat: lat,
-      lon: lon,
-      appid: apiKey,
-      lang: 'pt_br',
-      units: 'metrics',
-    }
+    inputBusca.style.display = 'flex';
+    btnBusca.style.display = 'flex';
+    adicionarLocal.style.display = 'flex';
   });
-  const responseData = response.data;
-  console.log(responseData)
-  nomeLocal.textContent = responseData.name;
 }
 
-const nomeLocal = document.querySelector('.nome-local');
+botaoOK.addEventListener('click', async (event) => {
+  event.preventDefault();
+  localidade = inputCriado.value;
+  if (!localidade) {
+    alert('Campo vazio, favor inserir um nome válido');
+    return;
+  }
+  const response = await localStorageReserv(localidade);
+  if (!response) {
+    localStorage.setItem("localidade", localidade);
+    return;
+  }
+});
 
+function fetchLocalPesquisa(value) {
+  if (!value) {
+    console.error('Campo de busca vazio');
+    alert("Campo de busca vazio!")
+    return;
+  }
+  apiData(value)
+}
+
+btnBusca.addEventListener('click', (e) => {
+  e.preventDefault();
+  fetchLocalPesquisa(inputBusca.value.trim());
+  inputBusca.value = '';
+});
+
+inputBusca.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter') {
+    fetchLocalPesquisa(inputBusca.value.trim());
+    inputBusca.value = '';
+  }
+});
+
+function renderizarLadoEsquerdo(response) {
+  const tempAPI = response.main.temp;
+  function renderTextosLadoEsquerdo() {
+    nomeLocal.textContent = response.name;
+    pais.textContent = response.sys.country;
+    descricaoClima.textContent = letraMaiuscula(response.weather[0].description);
+    temperaturaAtual.textContent = `${tempAPI.toFixed(1)}°C`;
+    ventos.textContent = `Ventos: ${response.wind.speed.toFixed(1)}Km/h`;
+    humidade.textContent = `Humidade: ${response.main.humidity.toFixed(1)}%`;
+    visibilidade.textContent = `Visibilidade: ${(response.visibility / 1000).toFixed(1)}Km`;
+    sensacaoTermica.textContent = `Sensação térmica: ${response.main.feels_like.toFixed(1)}°C`;
+  }
+  function imagensClima() {
+    const descricaoClimaAPI = response.weather[0].description;
+    const urlImagens = imagens.find(img => img.descricao.includes(descricaoClimaAPI));
+    const codigoIcon = response.weather[0].icon;
+    const dia = codigoIcon.includes('d');
+    if (dia) {
+      imgClima.src = urlImagens.iconDia;
+      ladoEsquerdoBackground.style.backgroundImage = `url('${urlImagens.giffDia}')`
+    } else {
+      imgClima.src = urlImagens.iconNoite;
+      ladoEsquerdoBackground.style.backgroundImage = `url('${urlImagens.giffNoite}')`;
+    }
+    if (tempAPI >= 0) {
+      imgPositivo.style.display = 'flex'
+      imgNegativo.style.display = 'none';
+    } else {
+      imgPositivo.style.display = 'none';
+      imgNegativo.style.display = 'flex';
+    }
+  }
+  renderTextosLadoEsquerdo();
+  imagensClima();
+}
+
+function letraMaiuscula(letra1) {
+  return letra1.charAt(0).toUpperCase() + letra1.slice(1);
+}
+
+apiData(localStorage.getItem("localidade"))
+renderizarAddClick();
+desrenderizarAddClick();
