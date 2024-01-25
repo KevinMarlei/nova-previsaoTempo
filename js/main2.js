a6dbf03570d54d6f55b18fa18b16554294b8d323
Kevin Marlei <kevin_SSDP@hotmail.com>
2024-01-25 18:13:18 +0100

    Adição de precipitação nas últimas 3 horas

--- js/main2.js
+++ js/main2.js
@@ -60,6 +60,7 @@ const sensacaoTermica = document.querySelector('.sensacao-termica');
 const ventos = document.querySelector('.vento');
 const humidade = document.querySelector('.humidade');
 const visibilidade = document.querySelector('.visibilidade');
+const ultiams3Horas = document.querySelector('.ultimas3Horas');
 //elementos criados dinâmicamente para favoritar local e renderizar dados ao iniciar seção
 const inputCriado = document.createElement('input');
 const botaoOK = document.createElement('button');
@@ -180,6 +181,7 @@ function renderizarLadoEsquerdo(response) {
     humidade.textContent = `Humidade: ${response.list[0].main.humidity.toFixed(1)}%`;
     visibilidade.textContent = `Visibilidade: ${(response.list[0].visibility / 1000).toFixed(1)}Km`;
     sensacaoTermica.textContent = `Sensação térmica: ${response.list[0].main.feels_like.toFixed(1)}°C`;
+    ultiams3Horas.textContent = `Chuvas nas últimas 3 horas: ${response.list[0].rain['3h']} mm`;
   }
   function imagensClima() {
     const descricaoClimaAPI = response.list[0].weather[0].description;
